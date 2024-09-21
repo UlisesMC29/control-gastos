7531b15f230c9a0ab405a4fdeea7525b23aa7d31
UlisesMC29 <contac_umc@hotmail.com>
2024-09-20 18:41:53 -0600

    Control de Gastos V1.1 Desafio 1 y 2 Añadido

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,56 @@
 // LISTAS O ARRAYS VACI@S
 let listaNombresGastos = [];
 let listaValoresGastos = [];
+let listaDescGastos = [];
 
 
 //FUNCION QUE INDICA LA ACCIÓN AL CLICKEAR EL BOTÓN
 function clickBoton(){
     let nombreGasto = document.getElementById('nombreGasto').value;//Método JS para obtener el valor de un elemento dentro del HTML
-    let valorGasto = document.getElementById('valorGasto').value;
+    let descGasto   = document.getElementById('descripcionGasto').value;
+    let valorGasto  = document.getElementById('valorGasto').value;
 
     console.log(nombreGasto + ': ' + valorGasto + ' USD')
     // console.log('Antes: ' + listaNombresGastos);
 
     listaNombresGastos.push(nombreGasto);//Función de JS para agregar elementos a una lista/array vací@ predefinid@
+    listaDescGastos.push(descGasto);
     listaValoresGastos.push(valorGasto);
     
-    console.log(listaNombresGastos);
-    console.log(listaValoresGastos);
+    // console.log(listaNombresGastos);
+    // console.log(listaValoresGastos);
+    // console.log(listaDescGastos);
 
+    if(Number(valorGasto) > 150){
+        Swal.fire({
+            title: "GASTASTE MÁS DE $150 USD &#128517;",
+            text: "Recuerda cuidar tus finanzas",
+            icon: "warning",
+            confirmButtonText: "Aceptar"
+          }).then((result)=> {
+                if(result.isConfirmed){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Gasto Agregado Correctamente",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+          });
+        }else{
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Gasto Agregado Correctamente",
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
     
     // alert('Funcionando');
     actualizarListaGastos();//Función que se ejecutara al clickear botón
+
 }
 
 //FUNCION PARA MOSTRAR LOS ELEMENTOS QUE SE ALMACENAN DENTRO DE LAS LISTAS
@@ -28,11 +59,11 @@ function actualizarListaGastos(){
     const totalElementos = document.getElementById('totalGastos');
     let htmlLista = ''; //variable vacía para que los elementos anteriores no queden almacenados y se pueda actualizar
     let totalGasto = 0;//variable con número 0 inicial para la suma de los gastos
-
     //bucle que leera un elemento por posición
     listaNombresGastos.forEach((element, position) => {
         const valorDGasto = Number(listaValoresGastos[position]); //Constante que contendrá el valor de la lista en la posición indicada
-        htmlLista += `<li>${element} - USD ${valorDGasto.toFixed(2)}
+        const descDGasto = listaDescGastos[position];
+        htmlLista += `<li>${element} (${descDGasto} ) - USD ${valorDGasto.toFixed(2)}
              <button onclick="eliminarGasto(${position});">Eliminar</button>
         </li>`;
         totalGasto += Number(valorDGasto);//método JS para convertir un valor string en un número (incluyendo decimales)
@@ -49,6 +80,7 @@ function actualizarListaGastos(){
 function limpiar() {
     document.getElementById('nombreGasto').value = ''; //con el método tomamos el valor de los inputs y los cambiamos por un valor vacio
     document.getElementById('valorGasto').value = '';
+    document.getElementById('descripcionGasto').value = '';
 }
 
 /**
@@ -61,6 +93,14 @@ function limpiar() {
 function eliminarGasto(position){
     listaNombresGastos.splice(position, 1);// función JS para eliminar elementos de una lista según la posición
     listaValoresGastos.splice(position, 1);
+
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Gasto Eliminado Correctamente",
+        showConfirmButton: false,
+        timer: 1500
+        });
 
     actualizarListaGastos();
 }
